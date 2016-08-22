14b0115616108d7ad99b3c5248d140b6cadb818e
fwtcumt <fanwentaocumt@163.com>
2016-08-22 09:18:48 +0800

    822 morning

--- src/scripts/views/detail.js
+++ src/scripts/views/detail.js
@@ -3,25 +3,29 @@ SPA.defineView("detail",{
 	//装载home视图
 	html:detailTpl,
 	//定义插件
-	plugins:[{
+	plugins:[
+		'delegated',
+		{
 		name:"avalon",
 		options:function(vm){
 			vm.livelist=[];
 		}
 	}],
 	//绑定视图事件
-	boindEvents:{
+	bindEvents:{
 		"show":function(){
 			//相应事件
+			console.log(1);
+			$("#detail-swiper").on('tap',function(){
+			})
+
+			var mySwiper=new Swiper('.lunbo',{
+				autoplay:2000,
+				pagination: '.swiper-pagination',
+				autoplayDisableOnInteraction : false,
+				loop:true
+			})
 		}
 	}
 
-})
-
-var mySwiper=new Swiper('.detail-container',{
-	autoplay:2000,
-	pagination: '.swiper-pagination'
-})
-$("#detail-swiper").on('tap',function(){
-	console.log(1);
 })
